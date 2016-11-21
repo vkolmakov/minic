488a6d96e47c49e28ea575a96ad77bff795ed410
vkolmakov <vkolmakov@hotmail.com>
2016-11-21 17:29:17 -0600

    Start working on grammar

---
+++ parser/parse_test.py
@@ -0,0 +1,17 @@
+import unittest
+from parser.parse import parser
+import parser.ast as ast
+
+
+class TestPrimitives(unittest.TestCase):
+    def test_id(self):
+        pass
+
+    def test_integer(self):
+        pass
+
+    def test_float(self):
+        pass
+
+    def test_arrayref(self):
+        pass
