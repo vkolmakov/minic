70ef364bd29298d088b1e8d3b533639c14c9b1f3
vkolmakov <vkolmakov@hotmail.com>
2016-11-22 18:31:58 -0600

    Add deep compare method for ast nodes

--- compiler/parser/ast.py
+++ compiler/parser/ast.py
@@ -1,6 +1,32 @@
 class AstNode:
     def __eq__(self, other):
-        pass
+        if not isinstance(other, AstNode):
+            return False
+
+        if not type(self) is type(other):
+            return False
+
+        for attr in list(self.__dict__):
+            # get top-level attribute values
+            self_data = self.__getattribute__(attr)
+            other_data = other.__getattribute__(attr)
+
+            if not type(self_data) is type(other_data):
+                return False
+
+            if hasattr(self_data, '__iter__') and hasattr(other_data, '__iter__'):
+                # if attribute is an iterable, check all subitems in order
+                for (self_subnode, other_subnode) in zip(self_data, other_data):
+                    if self_subnode.__ne__(other_subnode):
+                        return False
+            elif self_data.__ne__(other_data):
+                # if attribute if something else, check if they are equal
+                return False
+
+        return True
+
+    def __ne__(self, other):
+        return not (self == other)
 
 
 class Block(AstNode):
