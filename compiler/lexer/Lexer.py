a508c619024a7e4b160871bb1806e292fa1bd2ba
vkolmakov <vkolmakov@hotmail.com>
2016-11-24 10:58:00 -0600

    Resolve ambiguity b/w = and ==

--- compiler/lexer/Lexer.py
+++ compiler/lexer/Lexer.py
@@ -1,36 +1,37 @@
 from rply import LexerGenerator
 
 
-token_to_regex = {
-    'IF': r'if',
-    'ELSE': r'else',
-    'INT_TYPE': r'int',
+name_with_pattern = [
+    ('IF', r'if'),
+    ('ELSE', r'else'),
+    ('INT_TYPE', r'int'),
 
-    'LPAREN': r'\(',
-    'RPAREN': r'\)',
-    'LBRACE': r'\[',
-    'RBRACE': r'\]',
-    'LCURLY': r'\{',
-    'RCURLY': r'\}',
+    ('LPAREN', r'\('),
+    ('RPAREN', r'\)'),
+    ('LBRACE', r'\['),
+    ('RBRACE', r'\]'),
+    ('LCURLY', r'\{'),
+    ('RCURLY', r'\}'),
 
-    'SEMI': r';',
-    'COMMA': r',',
-    'EQUAL': r'=',
+    ('EQUAL_EQUAL', r'=='),
+    ('GREATER', r'>'),
+    ('GREATER_EQUAL', r'>='),
+    ('SMALLER', r'<'),
+    ('SMALLER_EQUAL', r'<='),
 
-    'EQUAL_EQUAL': r'==',
-    'GREATER': r'>',
-    'GREATER_EQUAL': r'>=',
-    'SMALLER': r'<',
-    'SMALLER_EQUAL': r'<=',
+    ('INTEGER', r'\d+'),
+    # ('FLOAT', r'\d+?\.\d+'),
+    ('ID', r'[a-zA-Z_][a-zA-Z0-9_]*'),
 
-    'INTEGER': r'\d+',
-    'ID': r'[a-zA-Z_][a-zA-Z0-9_]*',
+    ('PLUS', r'\+'),
+    ('MINUS', r'\-'),
+    ('MUL', r'\*'),
+    ('DIV', r'\/'),
 
-    'PLUS': r'\+',
-    'MINUS': r'\-',
-    'MUL': r'\*',
-    'DIV': r'\/',
-}
+    ('SEMI', r';'),
+    ('COMMA', r','),
+    ('EQUAL', r'='),
+]
 
 
 class Lexer:
@@ -40,7 +41,7 @@ class Lexer:
     def build_lexer(self):
         lg = LexerGenerator()
 
-        for (name, pattern) in token_to_regex.items():
+        for (name, pattern) in name_with_pattern:
             lg.add(name, pattern)
 
         lg.ignore(r'\s+')
@@ -49,7 +50,7 @@ class Lexer:
 
     @staticmethod
     def token_names():
-        return list(token_to_regex.keys())
+        return [name for (name, _) in name_with_pattern]
 
     def lex(self, source):
         stream = self.lexer.lex(source)
