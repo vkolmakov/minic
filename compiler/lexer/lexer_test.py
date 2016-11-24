89b72cdb5f859c1d39b2c4e35cf41be01d641d2b
vkolmakov <vkolmakov@hotmail.com>
2016-11-24 12:24:36 -0600

    Add test for keywords

--- compiler/lexer/lexer_test.py
+++ compiler/lexer/lexer_test.py
@@ -68,3 +68,30 @@ class TestSimpleStrings(unittest.TestCase):
 
         for (r, e) in zip_longest(result, expected):
             assert r == e
+
+
+class TestKeywords(unittest.TestCase):
+    def test_keywords(self):
+        '''Can distinguish between ids and keywords: `float x; int y; if duck else goose` '''
+        given = 'float x; int y; if duck else goose'
+
+        expected = iter([
+            Token('FLOAT_TYPE', 'float'),
+            Token('ID', 'x'),
+            Token('SEMI', ';'),
+
+            Token('INT_TYPE', 'int'),
+            Token('ID', 'y'),
+            Token('SEMI', ';'),
+
+            Token('IF', 'if'),
+            Token('ID', 'duck'),
+            Token('ELSE', 'else'),
+            Token('ID', 'goose'),
+        ])
+
+        result = lexer.lex(given)
+
+        for (r, e) in zip_longest(result, expected):
+            print(r, e)
+            assert r == e
