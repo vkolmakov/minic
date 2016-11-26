931dac4b427e3980978f6cebe2259c0542da0800
vkolmakov <vkolmakov@hotmail.com>
2016-11-25 20:30:20 -0600

    Add parser into compiler

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -1,9 +1,14 @@
 from compiler.lexer.lexer import Lexer
+from compiler.parser.parser import Parser
 
 
 class Compiler:
     def __init__(self):
         self.lexer = Lexer()
+        self.parser = Parser()
 
     def lex(self, source):
         return self.lexer.lex(source)
+
+    def parse(self, source):
+        return self.parser.parse(self.lex(source))
