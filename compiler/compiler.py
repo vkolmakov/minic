2531819e41ee58f5c98518991384029a50eed6d2
vkolmakov <vkolmakov@hotmail.com>
2016-11-25 20:41:42 -0600

    Pass the test

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -1,14 +1,19 @@
 from compiler.lexer.lexer import Lexer
 from compiler.parser.parser import Parser
+from compiler.typechecker.typechecker import Typechecker
 
 
 class Compiler:
     def __init__(self):
         self.lexer = Lexer()
         self.parser = Parser()
+        self.typechecker = Typechecker()
 
     def lex(self, source):
         return self.lexer.lex(source)
 
     def parse(self, source):
         return self.parser.parse(self.lex(source))
+
+    def typecheck(self, source):
+        return self.typechecker.typecheck(self.parse(source))
