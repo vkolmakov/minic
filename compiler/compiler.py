51615f4e6d66ff7b4a2bc5406ab98064f73859cf
vkolmakov <vkolmakov@hotmail.com>
2016-11-26 17:44:58 -0600

    Separate log and logging decorator

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -2,25 +2,23 @@ from compiler.lexer.lexer import Lexer
 from compiler.parser.parser import Parser
 from compiler.typechecker.typechecker import Typechecker
 
-from compiler.utils import Logger
 
-
-def create_compiler(with_logger=Logger()):
+def create_compiler(with_logger):
     class Compiler:
         def __init__(self):
             self.lexer = Lexer()
             self.parser = Parser()
             self.typechecker = Typechecker()
 
-        @with_logger.log('LEXER')
+        @with_logger.log_result('LEXER')
         def lex(self, source):
             return self.lexer.lex(source)
 
-        @with_logger.log('PARSER')
+        @with_logger.log_result('PARSER')
         def parse(self, source):
             return self.parser.parse(self.lex(source))
 
-        @with_logger.log('TYPECHECKER')
+        @with_logger.log_result('TYPECHECKER')
         def typecheck(self, source):
             return self.typechecker.typecheck(self.parse(source))
 
--- compiler/utils.py
+++ compiler/utils.py
@@ -6,22 +6,30 @@ def is_iterable(obj):
 
 
 class Logger:
-    def __init__(self, write_fn=print, to_file=None):
+    def __init__(self, write_fn=print, to_file=None):  # noqa
         if not to_file:
             self.write_output = write_fn
         else:
             file_obj = open(to_file, 'w+')
             self.write_output = file_obj.write
 
-    def log(self, event):
+    def log_result(self, event):
+        '''Used as a decorator'''
         def wrapper(f):
             def log_wrapped(*args, **kwargs):
                 msg = f(*args, **kwargs)
-                self.write_output(
-                    '[{}]-{}\n'.format(datetime.now(), event) +
-                    str(msg) +
-                    '\n'
-                )
+                self.log(event, msg)
                 return msg
             return log_wrapped
         return wrapper
+
+    def log(self, event, msg=''):
+        header = '[{}]-{}\n'.format(datetime.now(), event)
+        separator = '*' * (len(header) - 1) + '\n'
+        self.write_output(
+            separator +
+            header +
+            separator +
+            str(msg) +
+            '\n'
+        )
